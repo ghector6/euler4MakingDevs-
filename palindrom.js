159ff202df56373c4b0f179add1a9d42060c91bd
ghector6 <ghectormus@gmail.com>
2022-06-27 22:50:57 -0500

    adding functionality that prints all palidnrom from 2 digits

--- palindrom.js
+++ palindrom.js
@@ -1,9 +1,14 @@
 var res;
-for (let i = 1; i < 10; i++) {
-    for (let j =1; j < 10; j++) {
-        res = i + j;
-        let numStr = res.toString();
-        console.log(numStr.split("").reverse().join(""))
+for (let i = 10; i < 100; i++) {
+    for (let j =10; j < 100; j++) {
+        res = i * j;
+        let numStr = res.toString().split("").reverse().join("");
+        if (parseInt( numStr) === res){
+            console.log(numStr)
+        }
+
+        
+        // console.log(numStr.split("").reverse().join(""))
         // if (numStr.split("").reverse().join("") === res.toString()){
         //     console.log("here")
         // }
