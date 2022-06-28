358cd3bc21442054a3e7f167000d61e473d71a95
ghector6 <ghectormus@gmail.com>
2022-06-27 22:26:28 -0500

    adding split reverse and join to create an inverse value

--- palindrom.js
+++ palindrom.js
@@ -3,7 +3,11 @@ for (let i = 1; i < 10; i++) {
     for (let j =1; j < 10; j++) {
         res = i + j;
         let numStr = res.toString();
-        console.log(typeof(numStr));
+        console.log(numStr.split("").reverse().join(""))
+        // if (numStr.split("").reverse().join("") === res.toString()){
+        //     console.log("here")
+        // }
+        // console.log(typeof(numStr));
         // if ( res % 2 === 0){
         //     console.log(res)
         }
